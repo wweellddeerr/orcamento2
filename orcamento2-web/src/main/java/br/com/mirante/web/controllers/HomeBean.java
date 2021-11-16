6c48d7d559953a17c3b1b2012e21b2e6ca5fcc77
Welder Luz <pinheirol@tcu.gov.br>
2021-11-16 19:55:22 -0300

    evoluções encontro 1

--- orcamento2-web/src/main/java/br/com/mirante/web/controllers/HomeBean.java
+++ orcamento2-web/src/main/java/br/com/mirante/web/controllers/HomeBean.java
@@ -1,11 +1,30 @@
 package br.com.mirante.web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+
+import org.apache.log4j.Logger;
 
 @ManagedBean
 public class HomeBean {
+	
+	Logger logger = Logger.getLogger(HomeBean.class);
+	
+	public void digaOla() {
+		logger.info("Olá");
+	}
 
 	public String getMessage() {
 		return "Olá, mundo";
+	}
+	
+	public List<String> getOrcamentos() {
+		List<String> orcamentos = new ArrayList<>();
+		orcamentos.add("Orcamento 1");
+		orcamentos.add("Orcamento 2");
+		orcamentos.add("Orcamento 3");
+		return orcamentos;
 	}
 }
---
+++ orcamento2-web/src/main/java/br/com/mirante/web/controllers/TesteServlet.java
@@ -0,0 +1,20 @@
+package br.com.mirante.web.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mirante")
+public class TesteServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.getWriter().append("<html><body><h1>Olá mundo</h1></body></html>");
+	}
+	
+}
